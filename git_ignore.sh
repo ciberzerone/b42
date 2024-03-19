2a67e724640ca30db22c3f417c95f1402c2297a5
ciberzerone <gianmarcobeltran@gmail.com>
2024-03-19 18:58:44 +0100

    actualizado git_ignore.sh

--- git_ignore.sh
+++ git_ignore.sh
@@ -2,6 +2,7 @@
    
    # Lista todos los archivos ignorados por git que actualmente existen en tu repositorio local.
    git ls-files --others --ignored --exclude-standard
-   ssh-keygen -t ed25519 -C "gianmarcobeltran@gmail.com".pub
+   ciberzerone@gmail.com.pub
+   .DS_Store
 
    
