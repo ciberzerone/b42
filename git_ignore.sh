a36c65fd193c99aad08faa6cc92b6ff3282d3d7f
ciberzerone <gianmarcobeltran@gmail.com>
2024-03-19 18:16:12 +0100

    mensaje descritivo cambios

---
+++ git_ignore.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+   
+   # Lista todos los archivos ignorados por git que actualmente existen en tu repositorio local.
+   git ls-files --others --ignored --exclude-standard
+   ssh-keygen -t ed25519 -C "gianmarcobeltran@gmail.com".pub
+
+   
